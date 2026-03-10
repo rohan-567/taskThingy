045fc97b771695326c1a961e4be330329d2ea0de
rohan-567 <rohanadithyaakula@gmail.com>
2026-03-10 04:19:17 +0100

    workflow change

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "TaskThingy", home: MyHomePage());
+    return MaterialApp(title: "taskThingy", home: MyHomePage());
   }
 }
