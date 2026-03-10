1665bd40f1d8d248a20b347488bd27874257f0a2
rohan-567 <rohanadithyaakula@gmail.com>
2026-03-10 05:23:52 +0100

    workflow test #8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,14 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
 }
 
 flutter {
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+
+
 pluginManagement {
     val flutterSdkPath =
         run {
@@ -22,5 +24,6 @@ plugins {
     id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
+
 
 include(":app")
